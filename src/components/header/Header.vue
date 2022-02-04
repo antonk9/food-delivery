4bc7479f7d7186ac50a117caf61e0926156aab61
Anton <anton@yuush.com>
2022-02-04 19:56:50 +0100

    Init

---
+++ src/components/header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<header>
+			<SectionTitle title="Groceries" />
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SectionTitle from '@/components/header/SectionTitle.vue'
+
+export default defineComponent({
+	components: {
+		SectionTitle
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+	header {
+		padding: 16px;
+	}
+</style>
---
+++ src/components/header/SectionTitle.vue
@@ -0,0 +1,23 @@
+<template>
+	<h1>{{ title }}</h1>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue'
+
+	export default defineComponent({
+		name: 'SectionTitle',
+		props: {
+			title: {
+				type: String,
+				required: true
+			},
+		}
+	})
+</script>
+
+<style lang="scss" scoped>
+	h1 {
+		margin-bottom: 17px;
+	}
+</style>
